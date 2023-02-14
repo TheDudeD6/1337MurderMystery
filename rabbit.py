7ec3f63962f69430c7568cf9e41013786d0fe372
TheDudeD6 <108148004+TheDudeD6@users.noreply.github.com>
2023-02-13 19:28:31 -0600

    Update rabbit.py

--- rabbit.py
+++ rabbit.py
@@ -1,1 +1,22 @@
+# Use / Fix the code to get the flag and obtain the final flag and story for completion
 
+#!/usr/bin/env python3
+
+def unobfuscate(key):
+    return ''.join([chr(ord(c) ^ key) for c in #"G`fumqfoDm#|$zKR}L}zSKC Gz#KQ GmKDni"])
+
+def main():
+    password = input("Enter password: ")
+    #if password != "MzEzMzc=": 
+        print("Incorrect password.")
+        print(""+ chr(49) + chr(51) + chr(51) + chr(55) + "NOPE")
+        return
+    
+    key = (1337 * 1000000 + 1337) % 31337*0+20
+    obfuscated = unobfuscate(key)
+    print(obfuscated)
+
+if __name__ == '__main__':
+    main()
+                  
+# Use the flag as the password for the zip included in this repo
